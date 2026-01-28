172e8fe3e6e03dc6778b3f08ea55b2c1805fd47c
Dora-P1 <papaidora03@gmail.com>
2026-01-28 16:41:45 +0100

    "valq+ppquad"

---
+++ TP2/fonctions/valq.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/headers.h"
+
+int valq(int *t){
+    switch (*t) {
+        case 1 : 
+            q=9;
+            break;
+        case 2 :
+        case 3 : 
+            q=3;
+            break;
+
+    } 
+    return q;
+}
