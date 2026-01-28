b4ba6c0b7c1a908c6188776030d0fea81218d629
EhouarnDonniou <ehouarn.donniou@gmail.com>
2026-01-28 17:22:18 +0100

    "calFbase"

--- TP2/fonctions/calFbase.c
+++ TP2/fonctions/calFbase.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdlib.h>
-#include 
+#include "../include/headers.h"
 
-void calFbase(){
-
+//le vecteur float* valeurs a été calloc avant.
+//c-a-d, tout est initialisé à 0 dès le début. 
+void calFbase(int t, int* point, float** valeurs){
+    switch(t){
+        //quadrangles -> ordre 1 : Q1
+        case(1):
+            *valeur[0] = point[0]-point[0]*point[1];
+            *valeur[1] = point[0]*point[1];
+            *valeur[2] = point[1]-point[0]*point[1];
+            *valeur[3] = 1 - point[1]-point[0] + point[0]*point[1];
+        //triangles -> ordre 1 : P1
+        case(2):
+            *valeur[0] = point[0];
+            *valeur[1] = point[1];
+            *valeur[2] = 1-point[0]-point[1];
+        //segments
+        case(3):
+            *valeur[0] = *point;
+            *valeur[1] = -(*point)+1;
+    }
 }
-=======
-#include <stdlib.h>
->>>>>>> d87be4c ("structure TP2")
