48f58002f402de6283d9190b817b4e38b9ce6df8
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-01-29 23:25:57 +0100

    "modif verif. et lecfima"

--- TP1/fonctions/lecfima.c
+++ TP1/fonctions/lecfima.c
@@ -2,46 +2,37 @@
 #include <stdlib.h>
 #include "../include/headers.h"
 
-int lecfima(char* ficmai, int* ptypel, int* pnbtng, float*** pcoord, int* pnbtel, int*** pngnel, int* pnbneel, int* pnbaret, int*** pnRefAr){
+int lecfima(char *ficmai, int *ptypel, int *pnbtng, float ***pcoord, int *pnbtel, int ***pngnel, int *pnbneel, int *pnbaret, int ***pnRefAr){
+    printf("oui");
     FILE *pFile;
+    
     pFile = fopen(ficmai, "r");
     if(pFile == NULL){
         printf("Erreur d ouverture du fichier\n");
     return 1;
     }
-
     // Lecture de n
     fscanf(pFile,"%d",pnbtng);
     //Allouer le tableau pour stocker les coordonnees de dimension n x 2
-    float **pcoord1=alloctab(*pnbtng,2);
+    *pcoord=alloctab(*pnbtng,2);
     //Lexture des coordonnees
     for(int i=0;i<*pnbtng;i++){
         fscanf(pFile,"%f %f",&(*pcoord)[0][i],&(*pcoord)[1][i]);
     }
     //Lecture de pnbtel=m, ptypel=t, pnbneel=p, pnbaret=q
     fscanf(pFile,"%d %d %d %d",pnbtel,ptypel,pnbneel,pnbaret);
-
     //Allouer les tableaux pour pngnel de dimension mxp et pour pnRefAr de dimension mxq
-    int **pngel1=alloctabint(*pnbtel,*pnbneel);
-    int **pnRefAr1=alloctabint(*pnbtel,*pnbaret);
-
+    *pngnel=alloctabint(*pnbtel,*pnbneel);
+    *pnRefAr=alloctabint(*pnbtel,*pnbaret);
     //Lecture et remplissage des tableaux pngnel et pnRefAr
     for(int i=0; i<*pnbtel;i++){
         for(int j=0;j<*pnbneel;j++){
-            fscanf(pFile,"%d",pngel1[i][j]);
+            fscanf(pFile,"%d",&(*pngnel)[i][j]);
         }
         for(int j=0;j<*pnbaret;j++){
             fscanf(pFile,"%d",&(*pnRefAr)[i][j]);
         }
         
     }
-
-    ***pcoord = **pcoord1;
-    ***pngel1 = **pcoord1;
-    ***pnRefAr= **pnRefAr1;
-    //Liberes les tableaux
-    freetab(*pcoord);
-    freetab(*pngel1);
-    freetab(*pnRefAr);
     return 0;
 }
--- TP1/verification.c
+++ TP1/verification.c
@@ -2,30 +2,34 @@
 #include "include/errhandle.h"
 
 void main(){
-    char* ficmai = "outpu_maillage.txt";
-    float** pcoord;
-    int** pngnel;
-    int** pnRefAr;
-    int  typel, nbtng, nbtel, nbneel, nbaret;
-
-    int truc = lecfima(ficmai, &typel, &nbtng, &pcoord, &nbtel, &pngnel, &nbneel, &nbaret, &pnRefAr);
+    char *ficmai = "cad.txt";
+    float **pcoord;
+    int **pngnel, **pnRefAr;
+    int ptypel, pnbtng, pnbtel, pnbneel, pnbaret;
+    lecfima(ficmai,&ptypel,&pnbtng,&pcoord,&pnbtel,&pngnel, &pnbneel, &pnbaret, &pnRefAr);
 
     FILE* pFile;
     pFile = fopen("verif.txt", "w");
     if(pFile == NULL){
         printf("Erreur d'ouverture du fichier pour écriture\n");
-        exit(EXIT_FAILURE);
     }
-    fprintf(pFile,"%d\n",nbtng);
-    for(int i=0; i<nbtng;i++){
+    fprintf(pFile,"%d\n",pnbtng);
+    for(int i=0; i<pnbtng;i++){
         fprintf(pFile,"%f %f\n",pcoord[0][i],pcoord[1][i]);
     }
-    for(int i=0; i<nbtel;i++){
-        for(int j=0;j<nbneel;j++){
+
+    fprintf(pFile,"%d %d %d %d\n",pnbtel, ptypel, pnbneel, pnbaret);
+    for(int i=0; i<pnbtel;i++){
+        for(int j=0;j<pnbneel;j++){
             fprintf(pFile,"%d",pngnel[i][j]);
         }
-        for(int j=0;j<nbaret;j++){
+        for(int j=0;j<pnbaret;j++){
             fprintf(pFile,"%d",pnRefAr[i][j]);
         }
+        fprintf(pFile,"\n");
     }
+
+    freetab(pcoord);
+    freetab(pngnel);
+    freetab(pnRefAr);
 }
