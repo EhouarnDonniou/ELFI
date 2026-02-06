3ae49f07c3294ebeb803f3bbce9977f0610028de
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-02-06 14:17:58 +0100

    "invert M2x2 verif"

--- TP2/fonctions/invertM2x2.c
+++ TP2/fonctions/invertM2x2.c
@@ -17,20 +17,20 @@
 //
 //in : pointeur de matrice Mat pointant la matrice float 2x2 à inverser
 //     pointeur de matrice Mat_inv 2x2 float qui acceuillera l'inverse de Mat
-//out : float determiant, déterminant de Mat 
+//out : float determinant, déterminant de Mat 
 // edge case : 
 //      det(Mat)=0 => determinant = 0 et mat n'est pas inversée (Mat_inverse non-modifiée)
 float invertM2x2(float** Mat, float** Mat_inv){
-    float determiant = Mat[0][0]*Mat[1][1] - Mat[0][1]*Mat[1][0];
+    float determinant = Mat[0][0]*Mat[1][1] - Mat[0][1]*Mat[1][0];
     if(determinant<= 10e-16 ){
-        printf("/! ERROR \n La matrice n'est pas inversible!");
+        printf("/! ERROR \n La matrice n'est pas inversible!\n");
         return 0;
     }
 
-    Mat_inverse[0][0] = Mat[1][1]/determinant;
-    Mat_inverse[1][1] = Mat[0][0]/determinant;
-    Mat_inverse[0][1] = -Mat[0][1]/determinant;
-    Mat_inverse[1][0] = -Mat[1][0]/determinant;
+    Mat_inv[0][0] = Mat[1][1]/determinant;
+    Mat_inv[1][1] = Mat[0][0]/determinant;
+    Mat_inv[0][1] = -Mat[0][1]/determinant;
+    Mat_inv[1][0] = -Mat[1][0]/determinant;
 
     return(determinant);
 }
--- TP2/verifDamien.c
+++ TP2/verifDamien.c
@@ -1,11 +1,14 @@
 #include "include/headers.h"
 
 void main(){
-    float **A=alloctab(2,2);
-    float **Ainv=alloctab(2,2);
+    float det, **A, **Ainv;
+    A=alloctab(2,2);
+    Ainv=alloctab(2,2);
     A[0][0]=1;
     A[0][1]=1;
     A[1][0]=1;
     A[1][1]=1;
+    det = invertM2x2(A,Ainv);
     printtab(A,2,2);
+    printtab(Ainv,2,2);
 }
