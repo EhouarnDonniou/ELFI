fafcbfa29f04b97cdbf73cff1602d3765547f3dd
EhouarnDonniou <ehouarn.donniou@gmail.com>
2026-02-06 14:26:42 +0100

    truc truc

--- TP2/verifEhouarn.c
+++ TP2/verifEhouarn.c
@@ -2,29 +2,34 @@
 
 void main(){
     int t = 1;
-    //chaques sommets de K_ref (dim1 : i ; dim2 : j)
-    float** x_ref_q = alloctab(2, 1);
-    //sommet local 1
-    *x_ref_q[0] = 1; *x_ref_q[1] = 0;
-    printf("x-ref 1 : \n");
-    printtab(x_ref_q,2,1);
-    printf("%f %f", *x_ref_q[0] , *x_ref_q[1]);
+    //chaques sommets de K_ref 
+    float* x_ref_q = malloc(2*sizeof(float));
+    float* valeurs = malloc(4*sizeof(float));
 
+    //sommet local 1
+    x_ref_q[0] = 1; x_ref_q[1] = 0;
+    printf("x-ref 1 : \n");
+    calFbase(t,x,valeurs);
+    printf("wi(x-ref) 1 : \n");
+    
 
     //sommet local 2
-    *x_ref_q[0] = 1; *x_ref_q[1] = 1;
+    x_ref_q[0] = 1; x_ref_q[1] = 1;
     printf("x-ref 2 : \n");
-    printtab(x_ref_q,2,1);
+    calFbase(t,x,valeurs);
+    printf("wi(x-ref) 2 : \n");
 
     //sommet local 3
-    *x_ref_q[0] = 0; *x_ref_q[1] = 1;
+    x_ref_q[0] = 0; x_ref_q[1] = 1;
     printf("x-ref 3 : \n");
-    printtab(x_ref_q,2,1);
+    calFbase(t,x,valeurs);
+    printf("wi(x-ref) 3 : \n");
 
     //sommet local 4
-    *x_ref_q[0] = 1; *x_ref_q[0] = 1;
+    x_ref_q[0] = 0; x_ref_q[0] = 0;
     printf("x-ref 4 : \n");
-    printtab(x_ref_q,2,1);
+    calFbase(t,x,valeurs);
+    printf("wi(x-ref) 4 : \n");
 
 
     // t = 2;
