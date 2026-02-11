9537714d4065e2dfc4aaf7b81c2e2964536a172b
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-02-11 15:33:59 +0100

    "aa"

---
+++ TP3/fonctions/W.c
@@ -0,0 +1,13 @@
+/*
+--------------------------------------------------------------------------------
+    Ehouarn DONNIOU, Damien MALETTE, Dora PAPAI
+
+    Ecrit le 11/02/2026
+--------------------------------------------------------------------------------
+*/
+#include "include/headersTP3.h"
+void W(int nbneel, float *fctbase, float eltdif, float *vecelem){
+    for (int i=0; i<nbneel; i++){
+        
+    }
+}
