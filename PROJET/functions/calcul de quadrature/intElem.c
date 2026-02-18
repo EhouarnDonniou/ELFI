4db298086a5933edf838b93ea9c83d55a904b5d0
EhouarnDonniou <ehouarn.donniou@gmail.com>
2026-02-18 16:25:15 +0100

    intElem + check git works

--- PROJET/functions/calcul de quadrature/intElem.c
+++ PROJET/functions/calcul de quadrature/intElem.c
@@ -24,9 +24,11 @@ void intElem(int t, float** xquad, float* pdsquad, float** aK, float** matelm, f
     //nombre de points de quadrature en fonction du type.
     int q = valq(t);
 
+    float* wx_i = malloc(4*sizeof(float)); //taille 4 parce qu'on a max 4 fonctions de base
+    float** dwx_i = aloctab(2,4)
     //boucle sur les points de quadrature
     for(int i=0; i<q; i++){
-        float* wx_i
+        
         calFbase(t, x[i], wx_i)
     }
 }
