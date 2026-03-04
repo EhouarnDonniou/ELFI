888a943962e1e60a4545ccd3814d7410e2506c57
EhouarnDonniou <ehouarn.donniou@gmail.com>
2026-03-04 16:48:45 +0100

    à finir tkt

---
+++ TP2b/compil.sh
@@ -0,0 +1,6 @@
+gcc -o test_intElem.exe TP2b.c fonctions/cal1Elem.c 
+
+ - functions/calcul_quadrature/ADWDW.c functions/calcul_quadrature/FonctionDefinissantProbleme.c functions/calcul_quadrature/intElem.c
+ - functions/calcul_quadrature/ppquad.c functions/calcul_quadrature/W.c TP2b/functions/calcul_quadrature/WW.c
+ - functions/calculs_elementaires/Cal_fonc_bases.c functions/calculs_elementaires/gestion_matrices.c functions/calculs_elementaires/transFK.c
+ - 
