9ab69ce5a0ab9b0de6fe6818f36e0bccb1b7ef5b
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-03-04 16:23:34 +0100

    "TP2b ok2"

--- TP2b/TP2b.c
+++ TP2b/TP2b.c
@@ -19,18 +19,23 @@ void main(){
     float* NuDElem=calloc(nbneel,sizeof(float));
     float* uDElem=calloc(nbneel,sizeof(float));
     float** coorEl=alloctab(nbneel,nbneel);
-    //Initialisation à 0 de MatElem, SMbrElem, NuDElem et uDElem
-    for(int i=0; i<nbneel ; i++){
-        for(int j=0; j<nbneel; j++){
-            MatElem[i][j]=0;
-        }
-    }
-    
+
     //Boucle sur les éléments K
-    for(int i=0;i<k;i++){
-        selectPts(nbneel,ngnel[i],coord,coorEl);
+    for(int k=0;i<nbtel;i++){
+        
+        //Initialisation à 0 de MatElem, SMbrElem, NuDElem et uDElem
+        for(int i=0; i<nbneel ; i++){
+            SMbrElem[i]=0;
+            NuDElem[i]=0;
+            uDElem[i]=0;
+            for(int j=0; j<nbneel; j++){
+            MatElem[i][j]=0;
+            }
+        }
+        selectPts(nbneel,ngnel[k],coord,coorEl);
         cal1Elem(nRefDom, nbRefD0, numRefD0, nbRefD1, numRefD1, nbRefF1, numRefF1, 
                   typeEl, nbneel, coorEl, nbaret, nrefArEl,
                   MatElem, SMbrElem, NuDElem, uDElemen)
+        impCalEl(k, typEl, nbneel, MatElem, SMbrElem, NuDElem, uDElem) 
     }
 }
