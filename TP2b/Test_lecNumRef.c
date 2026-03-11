8328d5654325cd7da93c9af9ed1de75054dfccb0
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-03-11 15:47:45 +0100

    "LecNumref"

---
+++ TP2b/Test_lecNumRef.c
@@ -0,0 +1,12 @@
+/*
+--------------------------------------------------------------------------------
+    Damien MALETTE, Ehouarn DONNIOU, Dora PAPAI
+
+    Ecrit le 11/03/2026
+--------------------------------------------------------------------------------
+*/
+
+#include "include/headerTP1.h"
+#include "include/headerTP2a.h"
+#include "include/headerTP2b.h"
+#include "include/utilitaires.h"
--- TP2b/lecNumRef.c
+++ TP2b/lecNumRef.c
@@ -27,16 +27,16 @@ int lecNumRef(char* ficmai, int* nbrefdm,int* nbRefD0, int* nbRefD1, int* nbRefF
         fscanf(pFile,"%d",nbRefD0[i]);
     }
     //nombre de numeros de reference Dirichlet non homogene + tableau des numeros de reference Dirichlet non homogene
-    fscanf(pFile,"%d",nbRefD0);
-    numRefD0=calloc(nbRefD0,sizeof(int));
-    for(int i=0;i<nbRefD0;i++){
-        fscanf(pFile,"%d",nbRefD0[i]);
+    fscanf(pFile,"%d",nbRefD1);
+    numRefD1=calloc(nbRefD1,sizeof(int));
+    for(int i=0;i<nbRefD1;i++){
+        fscanf(pFile,"%d",nbRefD1[i]);
     }
-    fscanf(pFile,"%d",nbRefD0);
     //nombre de numeros de reference Neumann + tableau des numeros de reference Neumann
-    numRefD0=calloc(nbRefD0,sizeof(int));
-    for(int i=0;i<nbRefD0;i++){
-        fscanf(pFile,"%d",nbRefD0[i]);
+    fscanf(pFile,"%d",nbRefF1);
+    numRefD0=calloc(nbRefF1,sizeof(int));
+    for(int i=0;i<nbRefF1;i++){
+        fscanf(pFile,"%d",nbRefF1[i]);
     }
 
     return 0;
