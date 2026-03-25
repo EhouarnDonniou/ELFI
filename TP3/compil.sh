99aab5c74837812c1cdf813cc061ba5eba2f851f
EhouarnDonniou <ehouarn.donniou@gmail.com>
2026-03-25 15:46:17 +0100

    compil.sh de TP3 est bien

--- TP3/compil.sh
+++ TP3/compil.sh
@@ -1,3 +1,4 @@
 clear
-gcc TP2b.c functions/*.c functions/calcul_quadrature/*.c functions/calculs_elementaires/*.c functions/ecrit-lecture_maillage/*.c functions/fonctions_utilitaires/*.c -lm -o TP2b.exe
-TP2b.exe
+gfortran -c functions/fortran/*.f 
+gcc TP3.c *.o functions/*.c functions/calcul_quadrature/*.c functions/calculs_elementaires/*.c functions/ecrit-lecture_maillage/*.c functions/fonctions_utilitaires/*.c -lm -lgfortran -o TP3.exe
+TP3.exe
