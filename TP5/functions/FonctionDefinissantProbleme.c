7dc89e28fc5e0f0ed6ce3bb79bcd810310a8acc8
tabdamienmalette-droid <tab.damien.malette@gmail.com>
2026-05-06 15:53:18 +0200

    "def probleme"

--- TP5/functions/FonctionDefinissantProbleme.c
+++ TP5/functions/FonctionDefinissantProbleme.c
@@ -17,33 +17,111 @@ Contient les fonctions :
 // un point de coordonnées x=(x1,x2)
 
 float A11(float *x){
-    return 1;
+    return 1.;
 }
 
 float A22(float *x){
-    return 1;
+    return 1.;
 }
 
 float A12(float *x){
-    return 0;
+    return 0.;
 }
 
 float A00(float *x){
-    return 1;
+    float val = 0.;
+
+    switch (nucas) {
+        case 1 :
+	    val=0.;
+	    break;
+        case 2 :
+	    val=0.;
+	    break;
+        case 3 :
+	    val=1.;
+	    break;
+        default :
+	    printf("*** A00 : exemple non prevu.\n");
+	    break;
+    }
+  return(val);
 }
 
 float BN(float *x){
-    return 1;
+    return 0.;
 }
 
 float FOMEGA(float *x){
-    return 1;
+    const float PI = M_PI;
+    float val = 0.;
+
+    switch (nucas) {
+        case 1 :
+	    val=32.*(x[1]*x[1]+x[0]*x[0]-x[1]-x[0]);
+	    break;
+        case 2 :
+	    val=2.*PI*PI*sin(PI*x[0])*sin(PI*x[1]);
+	    break;
+        case 3 :
+	    val=(1.-2.*PI*PI)cos(PI*x[0])*cos(PI*x[1]);
+	    break;
+        default :
+	    printf("*** FOMEGA : exemple non prevu.\n");
+	    break;
+  }
+  return(val);
 }
 
 float FN(float *x){
-    return 1;
+    const float PI = M_PI;
+    float val = 0.;
+
+    switch (nucas) {
+        case 1 :
+	    val=0.;
+	    break;
+        case 2 :
+	    val=0.;
+	    break;
+        case 3 :
+	    if(x[1]==0.){
+            val=0.;
+        }
+        if(x[0]==0.){
+            val=0.;
+        }
+        if(x[1]==1. || x[1]==1/3){
+            val=-PI*cos(PI*x[0])*sin(PI*x[1]);
+        }
+        if(x[0]==1. || x[0]==1/3 ){
+            val=-PI*sin(PI*x[0])*cos(PI*x[1]);
+        }
+	    break;
+        default :
+	    printf("*** FN : exemple non prevu.\n");
+	    break;
+    }
+  return(val);
 }
 
 float UD(float *x){
-    return 100*x[0]+x[1];
+    const float PI = M_PI;
+    float val = 0.;
+
+    switch (nucas) {
+        case 1 :
+	    val=16.*x[0]*x[1]*(x[0]-1)*(1-x[1]);
+	    break;
+        case 2 :
+	    val=sin(PI*x[0])*sin(PI*x[1]);
+	    break;
+        case 3 :
+	    val=cos(PI*x[0])*cos(PI*x[1]);
+	    break;
+        default :
+	    printf("*** UD : exemple non prevu.\n");
+	    break;
+    }
+  return(val);
 }
